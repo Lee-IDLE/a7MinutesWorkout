8c7b1f55b1b5bc6363924752e9a759cdb1155caf
Lee-IDLE <nidus0526@gmail.com>
2023-11-26 22:07:29 +0900

    뷰바인딩 하는데 안 된다.. ㅜㅜ

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+//import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.infalte(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(this@MainActivity, "잘 뜬다", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
