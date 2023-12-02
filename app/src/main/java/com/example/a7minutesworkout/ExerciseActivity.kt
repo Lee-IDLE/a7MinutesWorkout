57cca5d49e03f987fb1fbbaa398c2a368d463e22
Lee-IDLE <nidus0526@gmail.com>
2023-12-03 01:05:22 +0900

    뷰바인딩 완료 했고 타이머가 끝나면 운동시간 타이머가 돌아가도록 기능 추가함

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -2,12 +2,19 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -16,18 +23,92 @@ class ExerciseActivity : AppCompatActivity() {
         setSupportActionBar(binding?.toolbarExerciese)
 
         if(supportActionBar != null){
-            binding?.toolbarExerciese?.setNavigationIcon(R.drawable.ic_button_back)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            binding?.toolbarExerciese?.title = "타이머"
         }
 
         binding?.toolbarExerciese?.setNavigationOnClickListener{
             finish()
         }
         this.onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+        setupRestView()
     }
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true){
         override fun handleOnBackPressed() {
             finish()
+        }
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setupExerciseView(){
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "Exercise Name"
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        setExerciseProgressBar()
+    }
+
+    private fun setRestProgressBar(){
+        binding?.ProgressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10 * 1000, 1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.ProgressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setExerciseProgressBar(){
+        binding?.ProgressBarExercise?.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30 * 1000, 1000){
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding?.ProgressBarExercise?.progress = 10 - exerciseProgress
+                binding?.tvTimerExercise?.text = (10 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,
+                    "30 Seconds are over, lets go to the rest view",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
         }
     }
 }
--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.a7minutesworkout
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.transition.Slide
+import android.view.Gravity
+import android.view.View
+import android.view.Window
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -11,14 +17,27 @@ import com.example.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        with(window){
+            requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+
+            enterTransition = Slide(Gravity.END)
+            enterTransition.duration = 700
+            exitTransition = Slide(Gravity.START)
+            exitTransition.duration = 700
+        }
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         binding?.flStart?.setOnClickListener {
             var intent = Intent(this@MainActivity, ExerciseActivity::class.java)
-            startActivity(intent)
+            startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this@MainActivity).toBundle())
         }
     }
 
