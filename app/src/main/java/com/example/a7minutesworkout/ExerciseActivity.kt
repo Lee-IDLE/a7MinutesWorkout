92e5eff91ae0abf38ae816c970f0cade94678d68
82102 <nidus0526@gmail.com>
2023-12-05 12:45:01 +0900

    쉬는 시간에 다음 운동이 뭐인지 알려주는 기능 추가

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -49,45 +49,55 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setupRestView(){
-        binding?.flRestView?.visibility = View.VISIBLE
-        binding?.tvTitle?.visibility = View.VISIBLE
-        binding?.tvExerciseName?.visibility = View.INVISIBLE
-        binding?.flExerciseView?.visibility = View.INVISIBLE
-        binding?.ivImage?.visibility = View.INVISIBLE
+        binding.flRestView?.visibility = View.VISIBLE
+        binding.tvTitle?.visibility = View.VISIBLE
+        binding.tvExerciseName?.visibility = View.INVISIBLE
+        binding.flExerciseView?.visibility = View.INVISIBLE
+        binding.ivImage?.visibility = View.INVISIBLE
+
+        binding.tvUpcomingLabel?.visibility = View.VISIBLE
+        binding.tvUpcomingExercise?.visibility = View.VISIBLE
+
         if(restTimer != null){
             restTimer?.cancel()
             restProgress = 0
         }
+
+        binding.tvUpcomingExercise?.text = exerciseList!![currentExercisePosition + 1].getName()
 
         setRestProgressBar()
     }
 
     // 운동 진행 바
     private fun setupExerciseView(){
-        binding?.flRestView?.visibility = View.INVISIBLE
-        binding?.tvTitle?.visibility = View.INVISIBLE
-        binding?.tvExerciseName?.visibility = View.VISIBLE
-        binding?.flExerciseView?.visibility = View.VISIBLE
-        binding?.ivImage?.visibility = View.VISIBLE
+        binding.flRestView?.visibility = View.INVISIBLE
+        binding.tvTitle?.visibility = View.INVISIBLE
+        binding.tvExerciseName?.visibility = View.VISIBLE
+        binding.flExerciseView?.visibility = View.VISIBLE
+        binding.ivImage?.visibility = View.VISIBLE
+
+        binding.tvUpcomingLabel?.visibility = View.INVISIBLE
+        binding.tvUpcomingExercise?.visibility = View.INVISIBLE
+
         if(exerciseTimer != null){
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
 
-        binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
-        binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
+        binding.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
         setExerciseProgressBar()
     }
 
     // 휴식 진행 바
     private fun setRestProgressBar(){
-        binding?.ProgressBar?.progress = restProgress
+        binding.ProgressBar?.progress = restProgress
 
         restTimer = object : CountDownTimer(10 * 1000, 1000){
             override fun onTick(p0: Long) {
                 restProgress++
-                binding?.ProgressBar?.progress = 10 - restProgress
-                binding?.tvTimer?.text = (10 - restProgress).toString()
+                binding.ProgressBar?.progress = 10 - restProgress
+                binding.tvTimer?.text = (10 - restProgress).toString()
 
 
             }
@@ -100,13 +110,13 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setExerciseProgressBar(){
-        binding?.ProgressBarExercise?.progress = exerciseProgress
+        binding.ProgressBarExercise?.progress = exerciseProgress
 
         exerciseTimer = object : CountDownTimer(30 * 1000, 1000){
             override fun onTick(p0: Long) {
                 exerciseProgress++
-                binding?.ProgressBarExercise?.progress = 30 - exerciseProgress
-                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+                binding.ProgressBarExercise?.progress = 30 - exerciseProgress
+                binding.tvTimerExercise?.text = (30 - exerciseProgress).toString()
             }
 
             override fun onFinish() {
